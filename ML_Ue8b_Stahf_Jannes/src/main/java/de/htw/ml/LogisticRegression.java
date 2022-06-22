e27f44d56584409d781c7d5a99ab2f7f178dae2f
Jannes <jannes.luca@hotmail.com>
2022-06-22 12:42:43 +0200

    Uebung8b added

---
+++ ML_Ue8b_Stahf_Jannes/src/main/java/de/htw/ml/LogisticRegression.java
@@ -0,0 +1,134 @@
+package de.htw.ml;
+
+import org.jblas.FloatMatrix;
+
+/**
+ *
+ * @author Nico Hezel
+ */
+public class LogisticRegression {
+
+	protected int trainingIterations;
+	protected float learnRate;
+	protected float[] predictionRates;
+	protected float[] trainErrors;
+
+	public LogisticRegression(int trainingIterations, float learnRate) {
+		this.trainingIterations = trainingIterations;
+		this.learnRate = learnRate;
+	}
+
+	public FloatMatrix train(FloatMatrix xTest, FloatMatrix yTest, FloatMatrix xTrain, FloatMatrix yTrain) {
+		this.predictionRates = new float[trainingIterations];
+		this.trainErrors = new float[trainingIterations];
+
+		// initialize the weights
+		org.jblas.util.Random.seed(7);
+		FloatMatrix theta = FloatMatrix.rand(xTrain.getColumns(), 1);
+
+		// current training error
+		trainErrors[0] = cost(predict(xTrain, theta), yTrain);
+
+		// best combination of weights
+		FloatMatrix bestTheta = theta.dup();
+		float bestPredictionRate = predictionRates[0] = predictionRate(predict(xTest, theta), yTest);
+
+		// training
+		for (int iteration = 0; iteration < trainingIterations; iteration++) {
+			predictionRates[iteration] = predictionRate(predict(xTest, theta), yTest);
+			trainErrors[iteration] = cost(predict(xTrain, theta), yTrain);
+
+			if(predictionRates[iteration] > bestPredictionRate) {
+				bestPredictionRate = predictionRates[iteration];
+				bestTheta = theta;
+			}
+			theta = changeTheta(xTrain, yTrain, theta, learnRate);
+		}
+
+		return bestTheta;
+	}
+
+	private FloatMatrix changeTheta(FloatMatrix xTrain, FloatMatrix yTrain, FloatMatrix theta, float learnRate) {
+
+		FloatMatrix h = predict(xTrain, theta).sub(yTrain).transpose();
+
+		FloatMatrix newTheta = theta.sub(h.mmul(xTrain).mul(learnRate / yTrain.length));
+
+		return newTheta;
+	}
+
+	/**
+	 * Calculates a prediction of the input data X and the current weights theta
+	 *
+	 * @param x
+	 * @param theta
+	 * @return
+	 */
+	public static FloatMatrix predict(FloatMatrix x, FloatMatrix theta) {
+		FloatMatrix y;
+		if(x.columns != theta.rows) {
+			y = x.transpose().mmul(theta);
+		} else {
+			y = x.mmul(theta);
+		}
+
+		y = sigmoidi(y);
+
+		return y;
+	}
+
+	/**
+	 * Calculates the training error according to the logistical cost function or RMSE.
+	 *
+	 * @param prediction
+	 * @param y
+	 * @return
+	 */
+	public static float cost(FloatMatrix prediction, FloatMatrix y) {
+		return prediction.sub(y).norm1() / y.length;
+	}
+
+	/**
+	 * Calculates a prediction rate between the prediction and the desired result Y.
+	 *
+	 * @param prediction
+	 * @param y
+	 * @return
+	 */
+	public static float predictionRate(FloatMatrix prediction, FloatMatrix y) {
+		prediction = prediction.ge(0.5f);
+		float rate = prediction.sub(y).norm1();
+		rate = (y.length-rate)/y.length * 100;
+		return  rate;
+	}
+
+	/**
+	 * Prediction rates of the last training
+	 *
+	 * @return
+	 */
+	public float[] getPredictionRates() {
+		return predictionRates;
+	}
+
+	/**
+	 * error rates of the last training
+	 *
+	 * @return
+	 */
+	public float[] getTrainError() {
+		return trainErrors;
+	}
+
+	/**
+	 * Replaces the values in the Input Matrix with their sigmoid variant.
+	 *
+	 * @param input
+	 * @return
+	 */
+	public static FloatMatrix sigmoidi(FloatMatrix input) {
+		for (int i = 0; i < input.data.length; i++)
+			input.data[i] = (float) (1. / ( 1. + Math.exp(-input.data[i]) ));
+		return input;
+	}
+}
---
+++ ML_Ue8b_Stahf_Jannes/src/main/java/de/htw/ml/data/CreditDataset.java
@@ -0,0 +1,199 @@
+package de.htw.ml.data;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.IntStream;
+
+import org.jblas.FloatMatrix;
+
+/**
+ * There are a lot TODOs here.
+ * The class divides the german credit dataset into train and test data.
+ *
+ * @author Nico Hezel
+ */
+public class CreditDataset implements Dataset {
+
+	protected Random rnd = new Random(7);
+
+	protected FloatMatrix xTrain;
+	protected FloatMatrix yTrain;
+
+	protected FloatMatrix xTest;
+	protected FloatMatrix yTest;
+
+	protected int[] categories;
+
+	public CreditDataset() throws IOException {
+
+		int predictColumn = 15; // typ of apartment
+		FloatMatrix data = FloatMatrix.loadCSVFile("german_credit_jblas.csv");
+
+		// List with all categories in the predictColumn
+		final FloatMatrix outputData = data.getColumn(predictColumn);
+		categories = IntStream.range(0, outputData.rows).map(idx -> (int)outputData.data[idx]).distinct().sorted().toArray();
+		int[] categorySizes = IntStream.of(categories).map(v -> (int)outputData.eq(v).sum()).toArray();
+		System.out.println("The unique values of y are "+ Arrays.toString(categories)+" and there number of occurrences are "+Arrays.toString(categorySizes));
+
+		// Array with all rows that are not predictColumn
+		int[] xColumns = IntStream.range(0, data.columns).filter(value -> value != predictColumn).toArray();
+
+		// Input and output data
+		FloatMatrix x = data.getColumns(xColumns);
+		FloatMatrix y = data.getColumn(predictColumn);
+
+		// min and maximum for all columns
+		FloatMatrix xMin = x.columnMins();
+		FloatMatrix xMax = x.columnMaxs();
+
+		// normalize the data sets and add the bias column
+		FloatMatrix xNorm = x.subRowVector(xMin).diviRowVector(xMax.sub(xMin));
+		xNorm = FloatMatrix.concatHorizontally(FloatMatrix.ones(xNorm.rows, 1), xNorm);
+
+		int testDataPerCategory = data.getRows() / 10 / categories.length; // 10% test set
+		int testDataCount = testDataPerCategory * categories.length;
+		System.out.println("Use "+testDataCount+" as test data with "+testDataPerCategory+" elements per category.\n");
+
+
+		divideData(xNorm, y, testDataPerCategory);
+	}
+
+	/**
+	 * Takes all data and divides it into a training and test set. Ultimately writes the values into
+	 * the classes fields holding the 4 corresponding matrices.
+	 *
+	 * @param xNorm normalized x values
+	 * @param y y values
+	 * @param testDataPerCategory how many data for each (of 3) catergories
+	 * @author Jannes Stahf
+	 */
+	private void divideData(FloatMatrix xNorm, FloatMatrix y, int testDataPerCategory) {
+
+		int y1 = 0;
+		int y2 = 0;
+		int y3 = 0;
+
+		// the following reduces in-loop-code by a few lines
+		yTrain = y.getRow(0);
+		xTrain = xNorm.getRow(0);
+
+		for(int i = 1; i<xNorm.rows; i++) {
+
+			switch((int) y.get(i)) {
+				case 1:
+					y1++;
+					break;
+				case 2:
+					y2++;
+					break;
+				case 3:
+					y3++;
+					break;
+			}
+
+			if((y.get(i) == 1 && y1>testDataPerCategory) || (y.get(i) == 2 && y2>testDataPerCategory) || (y.get(i) == 3 && y3>testDataPerCategory)){
+				yTrain = FloatMatrix.concatVertically(yTrain, y.getRow(i));
+				xTrain = FloatMatrix.concatVertically(xTrain, xNorm.getRow(i));
+			} else {
+				if (yTest == null) {
+					yTest = y.getRow(i);
+					xTest = xNorm.getRow(i);
+				} else {
+					yTest = FloatMatrix.concatVertically(yTest, y.getRow(i));
+					xTest = FloatMatrix.concatVertically(xTest, xNorm.getRow(i));
+				}
+			}
+		}
+	}
+
+	public int[] getCategories() {
+		return categories;
+	}
+
+	/**
+	 * The train data should contain as many train entries as possible but the ration
+	 * between data points of the desired category and data points of a different category
+	 * should be 50:50. All Y data are binarized:
+	 *  - desired category = 1
+	 *  - other category = 0
+	 *
+	 * @param category
+	 * @return {x Matrix,y Matrix}
+	 * @author Nico Hezel & Jannes Stahf
+	 */
+	public Dataset getSubset(int category) {
+
+		// Search as many other lines with a different category. Remove indices if
+		// necessary, to ensure the size of both set are the same
+
+		List<Integer> desiredIndices = new ArrayList<>();
+		List<Integer> otherIndices = new ArrayList<>();
+
+		// check for desired category and save index
+		for(int i = 0; i<yTrain.length; i++) {
+			if(yTrain.get(i) == category) {
+				desiredIndices.add(i);
+			} else {
+				otherIndices.add(i);
+			}
+		}
+
+		// if one set is bigger -> make them 50:50
+		if(desiredIndices.size()>otherIndices.size()) {
+			desiredIndices = desiredIndices.subList(0, otherIndices.size());
+		} else {
+			otherIndices = otherIndices.subList(0, desiredIndices.size());
+		}
+
+		otherIndices.addAll(desiredIndices); // join them together
+
+		int[] rowIndizies = otherIndices.stream().mapToInt(i -> i).toArray(); // convert to int[]
+
+		// Get the desired data points and binarize the Y-values
+		return new Dataset() {
+
+			@Override
+			public FloatMatrix getXTrain() {
+				return xTrain.getRows(rowIndizies);
+			}
+
+			@Override
+			public FloatMatrix getYTrain() {
+				return yTrain.getRows(rowIndizies).eq(category);
+			}
+
+			@Override
+			public FloatMatrix getXTest() {
+				return xTest;
+			}
+
+			@Override
+			public FloatMatrix getYTest() {
+				return yTest.eq(category);
+			}
+		};
+	}
+
+	@Override
+	public FloatMatrix getXTrain() {
+		return xTrain;
+	}
+
+	@Override
+	public FloatMatrix getYTrain() {
+		return yTrain;
+	}
+
+	@Override
+	public FloatMatrix getXTest() {
+		return xTest;
+	}
+
+	@Override
+	public FloatMatrix getYTest() {
+		return yTest;
+	}
+}
